e63188cb1a19ac0658d27ae04580621e29e3f880
maxmudjon101 <149583033+maxmudjon101@users.noreply.github.com>
2023-11-01 17:17:29 +0500

    gtvuvtuyugy
    huuiijioipo

---
+++ 10-dars.py
@@ -0,0 +1,57 @@
+# print("pirint salom")
+
+# dostlar=["daleer","ali","hasan","husen"]
+#
+# print(dostlar[0])
+# print(dostlar[1])
+# print(dostlar[2])
+# print(dostlar[3])
+#
+# print(dostlar[-1])
+#
+# mevalar=["olama","anor","ananas","banan","murot",]
+
+malumotlar={"isim":"alijon","yosh":16}
+
+
+print(malumotlar["isim"])
+print(malumotlar["yosh"])
+
+ism=malumotlar["isim"]
+yosh=malumotlar["yosh"]
+print(f"siznig ismingiz {ism} yoshingiz {yosh}da")
+
+#
+# dostlar={"ali":25,"husen":18, "amir":16, "xasan":15,"muslim":16 }
+#
+# dostlar["husen"]=12
+# print(dostlar)
+# del dostlar["ali"]
+# print(dostlar)
+#
+
+# print(dostlar)
+davlatlar={
+    "aqsh":"vashingtin",
+    "uzbekiston":"toshkent",
+    "italia":"rim",
+    "rosia":"moskova",
+    "ukraina":"kiyev",
+
+    }
+for kalit,qiymati in davlatlar.items():
+     print(f"{kalit} ning poytaxti {qiymati}")
+
+# isim("siznig ismingis nima yoshingiz nechida")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 12-dars.py
@@ -0,0 +1,9 @@
+malumotlar={"isim":"maxmdjon","yosh":25}
+
+
+print(malumotlar["isim"])
+print(malumotlar["yosh"])
+
+ism=malumotlar["isim"]
+yosh=malumotlar["yosh"]
+print(f"siznig ismingiz {ism} yoshingiz {yosh}da")
